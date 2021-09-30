0e3c3d24a5df28622dcd3f7a18e482709a649bf2
Sumit <tonystark83033@gmail.com>
2021-09-30 13:47:52 +0530

    new file:   ngo/db.sqlite3 	new file:   ngo/manage.py 	new file:   ngo/ngo/__init__.py 	new file:   ngo/ngo/__pycache__/__init__.cpython-39.pyc 	new file:   ngo/ngo/__pycache__/settings.cpython-39.pyc 	new file:   ngo/ngo/__pycache__/urls.cpython-39.pyc 	new file:   ngo/ngo/__pycache__/wsgi.cpython-39.pyc 	new file:   ngo/ngo/asgi.py 	new file:   ngo/ngo/settings.py 	new file:   ngo/ngo/urls.py 	new file:   ngo/ngo/wsgi.py 	new file:   ngo/ngosite/__init__.py 	new file:   ngo/ngosite/__pycache__/__init__.cpython-39.pyc 	new file:   ngo/ngosite/__pycache__/admin.cpython-39.pyc 	new file:   ngo/ngosite/__pycache__/apps.cpython-39.pyc 	new file:   ngo/ngosite/__pycache__/models.cpython-39.pyc 	new file:   ngo/ngosite/__pycache__/urls.cpython-39.pyc 	new file:   ngo/ngosite/__pycache__/views.cpython-39.pyc 	new file:   ngo/ngosite/admin.py 	new file:   ngo/ngosite/apps.py 	new file:   ngo/ngosite/migrations/__init__.py 	new file:   ngo/ngosite/migrations/__pycache__/__init__.cpython-39.pyc 	new file:   ngo/ngosite/models.py 	new file:   ngo/ngosite/static/ngosite/img/brsath.png 	new file:   ngo/ngosite/static/ngosite/img/chain.png 	new file:   ngo/ngosite/static/ngosite/img/cold.png 	new file:   ngo/ngosite/static/ngosite/img/cough.png 	new file:   ngo/ngosite/static/ngosite/img/covid.png 	new file:   ngo/ngosite/static/ngosite/img/desktop.ini 	new file:   ngo/ngosite/static/ngosite/img/dk.jpg 	new file:   ngo/ngosite/static/ngosite/img/fiver.png 	new file:   ngo/ngosite/static/ngosite/img/human-clipart-human-chain-18.png 	new file:   ngo/ngosite/static/ngosite/img/nose.png 	new file:   ngo/ngosite/static/ngosite/img/prevention.png 	new file:   ngo/ngosite/static/ngosite/img/tiredness.png 	new file:   ngo/ngosite/static/ngosite/img/virus.jpg 	new file:   ngo/ngosite/static/ngosite/index.css 	new file:   ngo/ngosite/static/ngosite/index.js 	new file:   ngo/ngosite/templates/ngosite/index.html 	new file:   ngo/ngosite/tests.py 	new file:   ngo/ngosite/urls.py 	new file:   ngo/ngosite/views.py

---
+++ ngo/ngosite/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NgositeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ngosite'
---
+++ ngo/ngosite/static/ngosite/index.js
@@ -0,0 +1,2 @@
+var c = new CountUp("total-case",0,29274823);
+c.start();
